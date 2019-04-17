ba4a6d0587f092b7186ea29c521d025686c7b76d
Catherine Cherubin <cat.cherubin@gmail.com>
2019-04-17 13:20:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
 
 var conversation = "Let\'s talk about " + `${topic}` + ".";
+
+//error greeting expected a string
+//concatenation of strings
+//interpolation of topic into the string conversation - use of placeholder syntax 
